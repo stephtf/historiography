b38d5e53fd542f7b8998c58e38e8e7f983cd608f
Stephanie Fajardo <stfajardo@gmail.com>
2022-02-09 21:14:24 -0500

    add user table to models

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const { Model, Datatypes } = require('sequelize'); 
+const sequelize = require('../config/connection'); 
+
+class User extends Model {} 
+
+User.init({
+    username: {
+        type: DataTypes.STRING, 
+    },
+    password: {
+        type: DataTypes.STRING,
+    },
+}, {
+    sequelize, 
+    timestamps: true,
+    underscored: true, 
+    freezeTableName: true, 
+    modelName: 'user',  
+}
+); 
+
+module.exports = User;

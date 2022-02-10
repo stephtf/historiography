8b9bc4e2a05d07d75ce66efe455cb4465b8e1a77
Stephanie Fajardo <stfajardo@gmail.com>
2022-02-09 21:19:32 -0500

    add the book table

---
+++ models/Book.js
@@ -0,0 +1,43 @@
+const { Model, Datatypes } = require('sequelize'); 
+const sequelize = require('../config/connection'); 
+
+class Book extends Model {} 
+
+Book.init({
+    field: {
+        type: DataTypes.STRING, 
+    },
+    title: {
+        type: DataTypes.STRING,
+    },
+    author: {
+        type: DataTypes.STRING,
+    },
+    argument: {
+        type: DataTypes.STRING,
+    },
+    examples: {
+        type: DataTypes.STRING,
+    },
+    keywords: {
+        type: DataTypes.STRING,
+    },
+    methods: {
+        type: DataTypes.STRING,
+    },
+    significance: {
+        type: DataTypes.STRING,
+    },
+    user_id: {
+        type: DataTypes.INT,
+    },
+}, {
+    sequelize, 
+    timestamps: true,
+    underscored: true, 
+    freezeTableName: true, 
+    modelName: 'user',  
+}
+); 
+
+module.exports = User;

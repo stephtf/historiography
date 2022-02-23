b4fb74296a223fdeb9eb62bc1ccad5033cbe862b
Stephanie Fajardo <stfajardo@gmail.com>
2022-02-23 14:33:46 -0500

    new login form

--- public/js/login.js
+++ public/js/login.js
@@ -26,3 +26,19 @@ document
     .getElementById('login-button')
     .addEventListener('click', loginForm);
 
+
+function password_show_hide() {
+    var x = document.getElementById("existing-password");
+    var show_eye = document.getElementById("show_eye");
+    var hide_eye = document.getElementById("hide_eye");
+    hide_eye.classList.remove("d-none");
+    if (x.type === "existing-password") {
+        x.type = "text";
+        show_eye.style.display = "none";
+        hide_eye.style.display = "block";
+    } else {
+        x.type = "password";
+        show_eye.style.display = "block";
+        hide_eye.style.display = "none";
+    }
+    }
